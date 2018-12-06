b522652c8280e15532b43b4f24484a06bc139779
duyangge <2380110794@qq.com>
2018-12-06 17:40:30 +0800

    dao层接口书写完毕

--- src/cn/jx/pxc/dao/UserDao.java
+++ src/cn/jx/pxc/dao/UserDao.java
@@ -3,14 +3,28 @@
  */
 package cn.jx.pxc.dao;
 
+import cn.jx.pxc.entity.User;
+
 /**
- *<p>Title: UserDao.java</p>
- *<p>Description: 描述</p>
+ *<p>Title: UserDao</p>
+ *<p>Description: User的dao层接口</p>
  * @package  cn.jx.pxc.dao
  * @author   黄信胜
  * @date     2018年12月5日下午1:02:29
- * @version 版本号
+ * @version  1.0
  */
 public interface UserDao {
-
+	/**
+	 * 添加用户
+	 * @param user 需要添加的实体类用户实体类
+	 * @return  添加的实体类用户实体类
+	 */
+	User addUser(User user);
+	
+	/**
+	 * 查询用户
+	 * @param user 需要查询的实体类用户实体类
+	 * @return 查询的实体类用户实体类或者为空
+	 */
+	User findUser(User user);
 }
--- src/cn/jx/pxc/dao/impl/UserDaoImpl.java
+++ src/cn/jx/pxc/dao/impl/UserDaoImpl.java
@@ -4,15 +4,34 @@
 package cn.jx.pxc.dao.impl;
 
 import cn.jx.pxc.dao.UserDao;
+import cn.jx.pxc.entity.User;
 
 /**
  *<p>Title: UserDaoImpl.java</p>
- *<p>Description: 描述</p>
+ *<p>Description: UserDao的接口实现类</p>
  * @package  cn.jx.pxc.dao.impl
  * @author   黄信胜
  * @date     2018年12月5日下午1:03:08
- * @version 版本号
+ * @version  1.0
  */
 public class UserDaoImpl implements UserDao {
+
+	/* (non-Javadoc)
+	 * @see cn.jx.pxc.dao.UserDao#addUser(cn.jx.pxc.entity.User)
+	 */
+	@Override
+	public User addUser(User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see cn.jx.pxc.dao.UserDao#findUser(cn.jx.pxc.entity.User)
+	 */
+	@Override
+	public User findUser(User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
