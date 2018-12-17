ba9732fc7a2693f2fd1956f02910e7d5765b4cde
Administrator <Administrator@SC-201706142333>
2018-12-17 22:34:02 +0800

    刘斐service

--- src/cn/jx/pxc/service/UserService.java
+++ src/cn/jx/pxc/service/UserService.java
@@ -3,14 +3,22 @@
  */
 package cn.jx.pxc.service;
 
+import cn.jx.pxc.entity.User;
+
 /**
  *<p>Title: UserService.java</p>
  *<p>Description: 描述</p>
  * @package  cn.jx.pxc.service
- * @author   黄信胜
- * @date     2018年12月5日下午1:03:53
+ * @author   刘斐
+ * @date     2018年12月17日22：33
  * @version 版本号
  */
 public interface UserService {
-
+	
+	//登录方法
+	User login(User user) throws Exception;
+	
+	//注册方法
+	void register(User user) throws Exception;
+	
 }
--- src/cn/jx/pxc/service/impl/UserServiceImpl.java
+++ src/cn/jx/pxc/service/impl/UserServiceImpl.java
@@ -3,14 +3,35 @@
  */
 package cn.jx.pxc.service.impl;
 
+import cn.jx.pxc.dao.UserDao;
+import cn.jx.pxc.entity.User;
+import cn.jx.pxc.service.UserService;
+
 /**
  *<p>Title: UserServiceImpl.java</p>
  *<p>Description: 描述</p>
  * @package  cn.jx.pxc.service.impl
- * @author   黄信胜
- * @date     2018年12月5日下午1:04:18
+ * @author   刘斐
+ * @date     2018年12月17日22：33
  * @version 版本号
  */
-public class UserServiceImpl {
+public class UserServiceImpl implements UserService{
+	
+	private UserDao userDao;
+	public void setUserDao(UserDao userDao) {
+		this.userDao = userDao;
+	}
 
+	//登录
+	public User login(User user) throws Exception {
+		
+		return userDao.findUser(user);
+	}
+
+	//注册
+	public void register(User user) throws Exception {
+		userDao.addUser(user);
+		
+	}
+	
 }
