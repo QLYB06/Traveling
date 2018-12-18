d406ef4842603507af69f06cfd162c499f35aba9
Administrator <Administrator@SC-201706142333>
2018-12-18 22:41:14 +0800

    刘斐12.18注释注解

--- src/cn/jx/pxc/service/UserService.java
+++ src/cn/jx/pxc/service/UserService.java
@@ -5,20 +5,27 @@ package cn.jx.pxc.service;
 
 import cn.jx.pxc.entity.User;
 
-/**
- *<p>Title: UserService.java</p>
- *<p>Description: 描述</p>
- * @package  cn.jx.pxc.service
- * @author   刘斐
- * @date     2018年12月17日22：33
- * @version 版本号
- */
-public interface UserService {
+/** *<p>Title: UserService.java</p> 
+ * *<p>Description: 描述</p> 
+ * * @package  cn.jx.pxc.service 
+ * * @author   刘斐 
+ * * @date     2018年12月17日22：33 
+ * * @version 版本号 */ 
+public interface UserService { 
+	/**
+	 * 调用dao查询用户方法
+	 * @param user user用户实体类
+	 * @return 查询的实体类用户实体类或者为空
+	 * @throws Exception
+	 */
+	User login(User user) throws Exception; 
 	
-	//登录方法
-	User login(User user) throws Exception;
-	
-	//注册方法
+	/**
+	 * 调用dao添加用户方法
+	 * @param user user用户实体类
+	 * @return  null
+	 * @throws Exception
+	 */
 	void register(User user) throws Exception;
 	
 }
--- src/cn/jx/pxc/service/impl/UserServiceImpl.java
+++ src/cn/jx/pxc/service/impl/UserServiceImpl.java
@@ -3,32 +3,37 @@
  */
 package cn.jx.pxc.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import cn.jx.pxc.dao.UserDao;
 import cn.jx.pxc.entity.User;
 import cn.jx.pxc.service.UserService;
 
-/**
- *<p>Title: UserServiceImpl.java</p>
- *<p>Description: 描述</p>
- * @package  cn.jx.pxc.service.impl
- * @author   刘斐
- * @date     2018年12月17日22：33
- * @version 版本号
- */
+/** *<p>Title: UserServiceImpl.java</p> 
+ * *<p>Description: 描述</p> 
+ * * @package  cn.jx.pxc.service.impl 
+ * * @author   刘斐 
+ * * @date     2018年12月17日22：33 
+ * * @version 版本号 */
+
+@Service("userService")
+@Transactional
 public class UserServiceImpl implements UserService{
-	
+	@Autowired
 	private UserDao userDao;
 	public void setUserDao(UserDao userDao) {
 		this.userDao = userDao;
 	}
 
-	//登录
+	
 	public User login(User user) throws Exception {
 		
 		return userDao.findUser(user);
 	}
 
-	//注册
+	
 	public void register(User user) throws Exception {
 		userDao.addUser(user);
 		
