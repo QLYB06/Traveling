15949c518ced65fcacf669e5eefe6645efbf0d05
孙立鑫 <@DESKTOP-0QPL3QM>
2019-01-14 14:10:49 +0800

    最新上传

--- src/cn/jx/pxc/entity/User.java
+++ src/cn/jx/pxc/entity/User.java
@@ -7,9 +7,9 @@ package cn.jx.pxc.entity;
  *<p>Title: UserInfo.java</p>
  *<p>Description: 描述</p>
  * @package  cn.jx.pxc.entity
- * @author   黄信胜
+ * @author   孙立鑫
  * @date     2018年12月5日下午12:57:22
- * @version 版本号
+ * @version 18.12.05
  */
 public class User{
 
